c3682db31fd41b4f93e1af942bf44fab8deb3e53
Dwayne Robinson <rdwayne876@gmail.com>
2023-07-17 19:33:55 -0500

    Basic nav header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,14 @@ import { MatListModule } from '@angular/material/list'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
