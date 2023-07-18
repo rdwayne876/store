90a274f95f107dac17af02245fc47ca10e3df4b5
Dwayne Robinson <rdwayne876@gmail.com>
2023-07-17 19:56:10 -0500

    sidebar layout

---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.component.html',
+
+})
+export class ProductsHeaderComponent {
+
+}
